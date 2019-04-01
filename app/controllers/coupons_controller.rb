6c6138e53646dfc14b1013ad450205e398c656a3
Rebecca Strauss <rstrauss127@gmail.com>
2019-04-01 21:39:38 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -2,9 +2,10 @@ class CouponsController < ApplicationController
 
   def new
   end
-      
+
   def create
     Coupon.create(coupon_code: params[:coupon_code], store: params[:store])
+    redirect_to Coupon.last
   end
 
   def show
